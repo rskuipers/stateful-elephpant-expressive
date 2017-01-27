554cd9f24c5d2d2ef4c2af70a3f03737d43bf8ed
Rick Kuipers <rick93@gmail.com>
2017-01-27 11:56:28 +0100

    Remove steps

--- src/App/Checkout/CheckoutActionFactory.php
+++ src/App/Checkout/CheckoutActionFactory.php
@@ -3,28 +3,14 @@ declare(strict_types=1);
 
 namespace App\Checkout;
 
-use App\Checkout\Steps\Details;
-use App\Checkout\Steps\Payment;
-use App\Checkout\Steps\Review;
-use Aura\Session\Session;
 use Interop\Container\ContainerInterface;
-use SM\Factory\Factory as StateMachineFactory;
 use Zend\Expressive\Router\RouterInterface;
-use Zend\Expressive\Template\TemplateRendererInterface;
 
 final class CheckoutActionFactory
 {
     public function __invoke(ContainerInterface $container): CheckoutAction
     {
-        $session = $container->get(Session::class);
-        $stateMachineFactory = $container->get(StateMachineFactory::class);
-        $orderStateMachine = $stateMachineFactory->get($session->getSegment('app')->get('order'));
-
-        $steps = [
-            new Details($container->get(TemplateRendererInterface::class)),
-            new Payment($container->get(TemplateRendererInterface::class)),
-            new Review($container->get(TemplateRendererInterface::class), $orderStateMachine),
-        ];
+        $steps = [];
 
         return new CheckoutAction($steps, $container->get(RouterInterface::class));
     }
